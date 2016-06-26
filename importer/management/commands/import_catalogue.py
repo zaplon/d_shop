96f579450cd3107883bfba96c1ac362d1c0bcc9a
jan <janek.zapal@gmail.com>
2016-06-26 23:05:08 +0200

    rozwoj

--- importer/management/commands/import_catalogue.py
+++ importer/management/commands/import_catalogue.py
@@ -4,14 +4,20 @@ import untangle
 from apps.catalogue.models import ProductClass, Product, ProductImage
 from oscar.apps.catalogue.categories import create_from_breadcrumbs
 from oscar.core.loading import get_model
+from django.core.exceptions import ObjectDoesNotExist
 import wget
+from django.conf import settings
 import os
-
 
 Category = get_model('catalogue', 'category')
 StockRecord = get_model('partner', 'stockrecord')
 Partner = get_model('partner', 'partner')
 ProductCategory = get_model('catalogue', 'productcategory')
+ProductAttribute = get_model('catalogue', 'productattribute')
+ProductAttributeValue = get_model('catalogue', 'productattributevalue')
+
+DOWNLOAD_FOLDER = os.path.join(settings.BASE_DIR, settings.OSCAR_IMAGE_FOLDER)
+FRONT_URL_ROOT = '/media/images/products/'
 
 
 class Command(BaseCommand):
@@ -20,6 +26,7 @@ class Command(BaseCommand):
     categories = {}
     breadcrumbs = []
     partner = Partner.objects.get(code='forcetop')
+    escape_map = {'ą': 'a', 'ę': 'e', 'ł': 'l', 'ś': 's', 'ć': 'c', 'ó': 'o', 'ż': 'z', 'ź': 'z'}
 
     def add_arguments(self, parser):
         # Named (optional) arguments
@@ -38,6 +45,12 @@ class Command(BaseCommand):
                             dest='delete-all',
                             help='Delete all before import')
 
+    def slugify(self, text):
+        text = text.replace(' ', '_').lower().encode('utf-8')
+        for e in self.escape_map:
+            text = text.replace(e, self.escape_map[e])
+        print text
+        return text
 
     def add_prices(self, p, node):
         try:
@@ -50,15 +63,19 @@ class Command(BaseCommand):
         sr.num_in_stock = 0 if node.stan.cdata < 5 else node.stan.cdata - 5
         sr.low_stock_threshold = 5
         sr.save()
-        
+
     def add_images(self, p, node):
         for i, zdjecie in enumerate(node.zdjecie):
             caption = p.title + ' ' + str(i)
-            #out = os.path.join(os.getcwd(), 'public', 'media', 'images', 'products', caption + '.jpg')
-            out = caption + '.jpg'
-            file_name = wget.download(zdjecie.cdata, out=out)
-            ProductImage.objects.create(product=p, original=file_name, caption=caption, display_order=i)
-            os.remove(file_name)
+            out = os.path.join(DOWNLOAD_FOLDER, caption + '.jpg')
+            front = os.path.join(FRONT_URL_ROOT, caption + '.jpg')
+            try:
+                file_name = wget.download(zdjecie.cdata, out=out)
+                ProductImage.objects.create(product=p, original=out, caption=caption, display_order=i)
+                # os.remove(file_name)
+                print file_name
+            except:
+                print 'No image  for product %s (%s)' % (p.title, i)
 
     def handle(self, *args, **options):
         # importowane klasy produktów
@@ -70,6 +87,7 @@ class Command(BaseCommand):
         delete_all = get_images = True
         if delete_all:
             Product.objects.all().delete()
+            ProductAttribute.objects.all().delete()
             Category.objects.all().delete()
             StockRecord.objects.all().delete()
         obj = untangle.parse(file)
@@ -93,11 +111,26 @@ class Command(BaseCommand):
             self.categories[breadcrumb_list[-1]].append(p.ProduktId)
             try:
                 p_obj = Product.objects.get(external_id=p.ProduktId.cdata)
-            except:
+            except ObjectDoesNotExist:
                 p_obj = Product(product_class=e_types[p_type])
             for f in self.fields_map:
                 setattr(p_obj, f, getattr(p, self.fields_map[f]).cdata)
             p_obj.save()
+            for a in p.atrybuty.atrybut:
+                if not a['name'] == 'Typ':
+                    try:
+                        pa = ProductAttribute.objects.get(product_class=p_obj.product_class, code=self.slugify(a.cdata))
+                    except ObjectDoesNotExist:
+                        pa = ProductAttribute.objects.create(product_class=p_obj.product_class,
+                                                             code=self.slugify(a['name']), type='text')
+                    pa.name = a['name']
+                    pa.save()
+                    try:
+                        pav = ProductAttributeValue.objects.get(product=p_obj, attribute=pa)
+                    except ObjectDoesNotExist:
+                        pav = ProductAttributeValue.objects.create(product=p_obj, attribute=pa)
+                    pav.value = a.cdata
+                    pav.save()
             self.stdout.write('Product %s saved' % p_obj.title)
             if get_images:
                 self.add_images(p_obj, p.zdjecia)
--- templatetags/templatetags/shop_tags.py
+++ templatetags/templatetags/shop_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from apps.catalogue.models import Product
+from apps.catalogue.models import Product, Category
 
 
 register = template.Library()
@@ -10,5 +10,13 @@ def product_box(slug):
     try:
         p = {'product': Product.objects.get(slug=slug)}
     except:
-        p = {}
+        p = {'product': Product.objects.all()[0]}
     return p
+
+
+@register.inclusion_tag('templatetags/box.html')
+def category_box(slug):
+    c = Category.objects.get(slug=slug)
+    image = getattr(c, 'image', None)
+    return {'title': c.name, 'url': c.get_absolute_url(), 'image': c.image.url if c.image else None}
+
