5f2f72252d4c7a55a0de2b0e7fcfb77b9b666b5c
zaplon <janek.zapal@gmail.com>
2016-08-18 10:39:16 +0200

    Update views.py

--- apps/catalogue/views.py
+++ apps/catalogue/views.py
@@ -105,16 +105,3 @@ class CatalogueView(TemplateView):
                                       'id': p.id, 'multiselect': True if p.code in ['kolor_bazowy'] else False}
                              for p in product_attributes}
         return ctx
-
-
-class ProductClassView(TemplateView):
-    context_object_name = "products"
-    template_name = "catalogue/etui.html"
-
-    def get_context_data(self, **kwargs):
-        ctx = {}
-        ctx['attributes'] = {p.name: {'values': p.productattributevalue_set.all().values(), 'id': p.id,
-                                      'multiselect': True if p.code in ['kolor_bazowy'] else False}
-                             for p in ProductAttribute.objects.filter(code__in=['wzor', 'kompatybilnosc',
-                                                                                'kolor_bazowy'])}
-        return ctx
