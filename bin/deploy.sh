931e7e6c0db63f1338d44cea951262b962697a63
jan <janek.zapal@gmail.com>
2016-09-10 17:03:36 +0200

    rozwoj

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-$ ssh zapalj@188.166.28.157 << EOF
-cd /var/www/dshop/
+PYTHON=~/.virtualenvs/shop/bin/python2.7
+ssh jan@188.166.28.157 << EOF
+cd /var/www/dshop/d_shop
 git pull
-workon shop
-./manage.py collectstatic
-./manage.py compress
+#static files
+printf 'yes' | $PYTHON manage.py collectstatic
+#compress js and css
+$PYTHON manage.py compress
+$PYTHON manage.py migrate
+service apache2 restart
 EOF
