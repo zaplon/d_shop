22eb1e860db045c9e9422a22484919a8314ab9db
zaplon <janek.zapal@gmail.com>
2016-09-18 12:47:46 +0200

    Update urls.py

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -6,6 +6,9 @@ urlpatterns = patterns(
      '',
      url(r'^products/$', views.ProductList.as_view(),
          name='product-list'),
+     url(r'^available_attributes/$', views.get_available_attributes,
+         name='available-attributes'),    
+         
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
