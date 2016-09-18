fba830d6991bb7509bebfa54d940a09e65ee2f07
zaplon <janek.zapal@gmail.com>
2016-09-18 17:17:42 +0200

    Update models.py

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -4,9 +4,6 @@ from oscar.apps.catalogue.abstract_models import AbstractProduct, AbstractProduc
 from oscar.core.loading import get_model
 
 
-ProductAttributeValue = get_model('catalogue', 'ProductAttributeValue')
-
-
 class Product(AbstractProduct):
     video_url = models.URLField(blank=True, null=True)
     external_id = models.IntegerField(blank=True, null=True)
@@ -15,11 +12,5 @@ class Product(AbstractProduct):
 class ProductClass(AbstractProductClass):
     external_type = models.CharField(max_length=50, blank=True, null=True)
 
-
-class ProductAttribute(AbstractProductAttribute):
-    def get_available_values(products):
-        ids = ProductAttributeValue.objects.filter(product__id__in=products, attribute=self).values_list('id', flat=True)
-        return ids
-    
 
 from oscar.apps.catalogue.models import *
