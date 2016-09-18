d480bd38e421943b04d4dc358a68ee658a4991e5
zaplon <janek.zapal@gmail.com>
2016-09-18 12:16:52 +0200

    Update models.py

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-from oscar.apps.catalogue.abstract_models import AbstractProduct, AbstractProductClass
+from oscar.apps.catalogue.abstract_models import AbstractProduct, AbstractProductClass, AbstractProductAttribute
+from oscar.core.loading import get_model
+
+
+ProductAttributeValue = get_model('catalogue', 'ProductAttributeValue')
 
 
 class Product(AbstractProduct):
@@ -11,5 +15,11 @@ class Product(AbstractProduct):
 class ProductClass(AbstractProductClass):
     external_type = models.CharField(max_length=50, blank=True, null=True)
 
+
+class ProductAttribute(AbstractProductAttribute):
+    def get_available_values(products):
+        ids = ProductAttributeValue.objects.filter(product__id__in=products, attribute=self).values_list('id', flat=True)
+        return ids
+    
 
 from oscar.apps.catalogue.models import *
