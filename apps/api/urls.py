2cecd1c8948374a15d3a3f52b7f9884b2191f9e1
jan <janek.zapal@gmail.com>
2016-10-10 22:49:45 +0200

    bugs fixing

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
+from django.views.decorators.cache import cache_page
+from django.conf import settings
+
 
 urlpatterns = patterns(
      '',
-     url(r'^products/$', views.ProductList.as_view(),
+     url(r'^products/$', cache_page(settings.CACHE_PERIOD)(views.ProductList.as_view()),
          name='product-list')
          
 )
