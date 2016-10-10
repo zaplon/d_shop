672eeadebb71c963603d9e6a209c2a4e6a1fd3b2
jan <janek.zapal@gmail.com>
2016-10-10 23:05:29 +0200

    bugs fixing

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,9 +4,9 @@ ssh jan@188.166.28.157 << EOF
 cd /var/www/dshop/d_shop
 git pull
 #static files
-printf 'yes' | $PYTHON manage.py collectstatic
+printf 'yes' | $PYTHON manage.py collectstatic --settings=production_settings
 #compress js and css
-$PYTHON manage.py compress
+$PYTHON manage.py compress --settings=production_settings
 touch shop/wsgi.py
-$PYTHON manage.py migrate
+$PYTHON manage.py migrate --settings=production_settings
 EOF
