e39949a42bd205a9459c0b24e485dacc20be47cc
zaplon <janek.zapal@gmail.com>
2016-11-17 14:42:35 +0100

    Update api.ts

--- staticfiles/js/search/api.ts
+++ staticfiles/js/search/api.ts
@@ -1,5 +1,20 @@
 /// <reference path="../jquery.d.ts" />
 
+// {
+//   "query": { 
+//     "bool": { 
+//       "must": [
+//         { "match": { "title":   "Search"        }}, 
+//         { "match": { "content": "Elasticsearch" }}  
+//       ],
+//       "filter": [ 
+//         { "term":  { "status": "published" }}, 
+//         { "range": { "publish_date": { "gte": "2015-01-01" }}} 
+//       ]
+//     }
+//   }
+// }
+
 class Search {
     params: any;
     getResults(){
