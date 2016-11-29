2602783a3185858d5fb42c57d4b1f866bf771042
zaplon <janek.zapal@gmail.com>
2016-11-29 14:53:44 +0100

    Update api.py

--- apps/search/api.py
+++ apps/search/api.py
@@ -13,9 +13,19 @@ Product = get_model('catalogue', 'Product')
 def update_product(id):
     p = Product.objects.get(id=id)
     data = JSONRenderer().render(ProductElasticSerializer(p).data)
-    res = requests.post('%s/%s' % (settings.ELASTIC_URL, p.external_id), data=data)
+    res = requests.post('%s/%s' % (settings.ELASTIC_URL + 'product/', p.external_id), data=data)
     if res.status_code >= 200 and res.status_code < 300:
         print '%s updated successfully' % p.title
     else:
         print res.__dict__
         print 'there was error updating %s' % p.title
+        
+def update_category(c):
+    data = JSONRenderer().render({'full_name': c.full_name, 'name': c.name})
+    res = requests.post('%s/%s' % (settings.ELASTIC_URL + 'category/' , c.id), data=data)
+    if res.status_code >= 200 and res.status_code < 300:
+        print '%s updated successfully' % c.name
+    else:
+        print res.__dict__
+        print 'there was error updating %s' % c.name
+
