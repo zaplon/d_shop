cedf70e0a6a5cb40b458a1ed2842d964aed7d7dd
zaplon <janek.zapal@gmail.com>
2016-12-06 11:48:07 +0100

    Update models.py

--- apps/partner/models.py
+++ apps/partner/models.py
@@ -3,7 +3,7 @@ from oscar.apps.partner.abstract_models import AbstractStockRecord
 
 class StockRecord(AbstractStockRecord):
     def is_available(self):
-        return self.num_in_stock > 1
+        return self.num_in_stock > 0
 
 
 from oscar.apps.partner.models import *
