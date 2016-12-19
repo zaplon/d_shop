f8a1d9cd9aee9054dc2aa90f66512a58a291026f
jan <janek.zapal@gmail.com>
2016-12-19 21:00:20 +0100

    katalog

--- apps/partner/models.py
+++ apps/partner/models.py
@@ -3,7 +3,7 @@ from oscar.apps.partner.abstract_models import AbstractStockRecord
 
 class StockRecord(AbstractStockRecord):
     def is_available(self):
-        return self.num_in_stock > 0
+        return 1 if self.num_in_stock > 0 else 0
 
 
 from oscar.apps.partner.models import *
