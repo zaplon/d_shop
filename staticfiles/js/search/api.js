62ac4728cde06d98c1a861d28df62bec7f1eb153
jan <janek.zapal@gmail.com>
2016-12-31 14:13:09 +0100

    js

--- staticfiles/js/search/api.js
+++ staticfiles/js/search/api.js
@@ -84,7 +84,7 @@ var Search = (function () {
         });
         me.filters = me.filters.slice(0, 6);
         me.filters.forEach(function (f) {
-            f.options = f.options.sort(function (a, b) { return a.text > b.text; });
+            f.options = f.options.sort(function (a, b) { return a.text > b.text ? 1 : -1; });
         });
         return { products: me.results, filters: me.filters, prices: [res.aggregations.min_price.value, res.aggregations.max_price.value],
             count: res.hits.total };
