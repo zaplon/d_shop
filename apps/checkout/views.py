ac96249506a7c4e0170dbf417b5ff117d1035b1f
zaplon <janek.zapal@gmail.com>
2017-01-31 12:40:36 +0100

    Update views.py

--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -1,4 +1,5 @@
 from oscar.apps.checkout.views import PaymentDetailsView
+from oscar.apps.payment import models
 
 
 class PaymentDetailsView(PaymentDetailsView):
@@ -13,3 +14,21 @@ class PaymentDetailsView(PaymentDetailsView):
         ctx = self.get_context_data(**kwargs)
         ctx['payment_method'] = request.POST['payment-method']
         return self.render_to_response(ctx)
+    
+    def handle_payment(self, order_number, total, **kwargs):
+        # Talk to payment gateway.  If unsuccessful/error, raise a
+        # PaymentError exception which we allow to percolate up to be caught
+        # and handled by the core PaymentDetailsView.
+        #reference = gateway.pre_auth(order_number, total.incl_tax, kwargs['bankcard'])
+
+        # Payment successful! Record payment source
+        source_type, __ = models.SourceType.objects.get_or_create(
+            name="Przy odbiorze")
+        source = models.Source(
+            source_type=source_type,
+            amount_allocated=0,
+            reference='a')
+        self.add_payment_source(source)
+
+        # Record payment event
+        self.add_payment_event('pre-auth', total.incl_tax)
